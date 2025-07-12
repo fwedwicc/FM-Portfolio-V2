78ea41f2c2ddf69cce15f137250fdc3b0c5281b2
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-07-12 21:44:13 +0800

    Enhance About component styling with updated GlowContainer effects; adjust Sidebar icons for improved visual consistency; modify CSS for better typography and add gradient backgrounds for glow effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { TbCode } from "react-icons/tb"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -68,13 +69,13 @@ const About = () => {
 
   const GlowContainer = ({ id, variant, children, styles }) => {
     return (
-      <div id={id} className={`relative group bg-[#262333] p-[1px] h-64 rounded-4xl ${styles}`}>
+      <div id={id} className={`relative group bg-[#1d1b28] p-[1px] rounded-4xl ${styles}`}>
         {/* Glow 3 - Inner */}
-        <div className={`absolute w-14 h-[170%] bg-indigo-500 opacity-0 group-hover:opacity-90 blur-[5rem] -translate-y-1/2 top-1/2 transition-smooth ${variant === 'left' ? 'left-[-9.5rem]' : 'right-[-9.5rem]'}`}></div>
+        <div className={`absolute w-14 h-[170%] bg-indigo-500 opacity-0 group-hover:opacity-90 blur-[5rem] -translate-y-1/2 top-1/2 transition-smooth delay-100 pointer-events-none ${variant === 'left' ? 'left-[-9.5rem]' : 'right-[-9.5rem]'}`}></div>
         {/* Glow 3 - Outer */}
-        <div className={`absolute w-10 h-[50%] bg-indigo-400 opacity-0 group-hover:opacity-90 blur-[2rem] -translate-y-1/2 top-1/2 transition-smooth ${variant === 'left' ? 'left-[-11rem]' : 'right-[-11rem]'}`}></div>
+        <div className={`absolute w-10 h-[50%] bg-indigo-400 opacity-0 group-hover:opacity-90 blur-[2rem] -translate-y-1/2 top-1/2 transition-smooth delay-100 pointer-events-none ${variant === 'left' ? 'left-[-12rem]' : 'right-[-12rem]'}`}></div>
         {/* Glow Border */}
-        <div className={`w-full h-full absolute top-0 left-0 from-[#191721] via-[#BFADEF] to-[#4F46E5] -z-10 rounded-4xl opacity-0 group-hover:opacity-100 transition-smooth ${variant === 'left' ? 'bg-gradient-to-l' : 'bg-gradient-to-r'}`}></div>
+        <div className={`w-full h-full absolute top-0 left-0 -z-10 rounded-4xl opacity-0 group-hover:opacity-100 transition-smooth delay-150 ${variant === 'left' ? 'hero-left-glow' : 'hero-right-glow'}`}></div>
         {/* Content */}
         {children}
       </div>
@@ -102,13 +103,25 @@ const About = () => {
         <GlowContainer id='left-container' variant='left'>
           <div className='p-8 w-full h-full bg-[#15131C] rounded-[31px]'>
             <div className='space-y-4'>
+
+              {/* <div className='relative inline-flex p-[1px] overflow-hidden rounded-[19px] group'>
+                <div className='absolute w-full h-full top-0 left-0 bg-gradient-to-l from-[#BFADEF] to-[#4F46E5] opacity-0 group-hover:opacity-100 transition-smooth delay-200'></div>
+                <div className='p-4 bg-[#15131C] z-10 rounded-[18px]'>
+                  <TbCode className='text-indigo-200/50 size-8 stroke-[1.2px]' />
+                </div>
+              </div> */}
+
+              <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] transition-smooth'>
+                <TbCode className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1.2px] transition-smooth' />
+              </div>
+
               <h2>i design</h2>
               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
             </div>
           </div>
         </GlowContainer>
         {/* 'i develop' */}
-        <div className={`relative group bg-[#262333] hover:bg-transparent p-[1px] h-64 rounded-4xl transition-smooth hover:shadow-2xl shadow-indigo-500/20`}>
+        <div className={`relative group bg-[#1d1b28] hover:bg-transparent p-[1px] h-64 rounded-4xl transition-smooth hover:shadow-2xl shadow-indigo-500/20`}>
           {/* Glow Border */}
           <div className={`w-full h-full absolute top-0 left-0 bg-gradient-to-b from-[#191721] via-[#BFADEF] to-[#4F46E5] -z-10 rounded-4xl opacity-0 group-hover:opacity-100 transition-smooth`}></div>
           {/* Content */}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { RiHome5Line, RiUser6Line, RiAwardLine, RiSendPlane2Line, RiBriefcaseLine } from "react-icons/ri"
+import { TbSmartHome, TbBriefcase2, TbMail } from "react-icons/tb"
 import useActiveSection from '../hooks/useActiveSection'
 import { Tooltip } from './ui'
 
@@ -16,20 +17,20 @@ const Sidebar = () => {
   const active = useActiveSection(['home', 'about', 'experience', 'project', 'contact'])
 
   const navItems = [
-    { id: 'home', label: 'Home', icon: <RiHome5Line className='size-5' /> },
-    { id: 'about', label: 'About', icon: <RiUser6Line className='size-5 stroke-[0.3px]' /> },
-    { id: 'experience', label: 'Experience', icon: <RiAwardLine className='size-5' /> },
-    { id: 'project', label: 'Projects', icon: <RiBriefcaseLine className='size-5' /> },
-    { id: 'contact', label: 'Contact', icon: <RiSendPlane2Line className='size-5' /> },
+    { id: 'home', label: 'Home', icon: <TbSmartHome className='size-5 stroke-[1.9px]' /> },
+    { id: 'about', label: 'About', icon: <RiUser6Line className='size-5 stroke-[-0.1px]' /> },
+    { id: 'experience', label: 'Experience', icon: <RiAwardLine className='size-5 stroke-[-0.3px]' /> },
+    { id: 'project', label: 'Projects', icon: <TbBriefcase2 className='size-5 stroke-[1.9px]' /> },
+    { id: 'contact', label: 'Contact', icon: <TbMail className='size-5 stroke-[1.9px]' /> },
   ]
 
   return (
     <aside className='px-6 border border-green-500/20 fixed h-screen flex items-center z-50'>
-      <div className='flex flex-col gap-1 border border-[#262333] bg-[#15131C] p-1 rounded-[18px]'>
+      <div className='flex flex-col gap-1 border border-[#1e1b29] bg-[#15131C] p-1 rounded-[18px]'>
         {navItems.map(({ id, icon, label }) => (
           <div className='inline-flex group relative transition-smooth' key={id}>
             <a href={`#${id}`} onClick={(e) => scrollToSection(e, `#${id}`)}
-              className={`size-12 p-[1px] rounded-[14px] group transition-smooth ${active === id ? 'bg-gradient-to-b from-[#BFADEF] to-[#4F46E5] text-white shadow-xl shadow-indigo-400/20' : 'bg-[#262333] text-indigo-200/40'
+              className={`size-12 p-[1px] rounded-[14px] group transition-smooth ${active === id ? 'bg-gradient-to-b from-[#BFADEF] to-[#4F46E5] text-white shadow-xl shadow-indigo-400/20' : 'bg-[#201e2c] text-indigo-200/40'
                 }`}
             >
               <div className={`w-full h-full grid place-content-center group-hover:bg-[#111017] rounded-[13px] transition-smooth ${active === id ? 'bg-[#111017]' : 'bg-[#17151E]'}`}>
