e86708438d9939a7901b01610a6ce760a342454c
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-07-22 22:26:34 +0800

    Refactor Contact and Experience components: enhance layout, add icons, and implement glow effects in styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,64 +1,77 @@
 import React from 'react'
+import { Button } from './ui'
 import { FMLogo } from '../assets'
 import { CertificationsGlow } from '../assets/experience'
+import { TbPhone, TbMail, TbMapPin, TbArrowUpRight } from "react-icons/tb"
 
 const Contact = () => {
   return (
     <section className='relative h-auto md:pt-24 pt-9 space-y-16 border border-orange-500/20' id='contact'>
       {/* Content */}
-      <div className='w-full flex justify-between items-start md:px-36 px-6 border'>
+      <div className='w-full flex justify-between items-end md:px-36 px-6'>
         {/* Left Contents */}
-        <div className='border'>
-          <h1 className='text-[60px]'>get in touch.</h1>
-          <p>moreno.frederick.capiral@gmail.com</p>
-          <p>Caloocan City</p>
+        <div className='space-y-7'>
+          <div className='flex flex-col space-y-1.5'>
+            <h1 className='text-[65px] mb-2'>get in touch.</h1>
+            {[
+              { label: 'moreno.frederick.capiral@gmail.com', icon: <TbMail className='size-4.5 stroke-[1.4px]' /> },
+              { label: '+63 921 432 3915', icon: <TbPhone className='size-4.5 stroke-[1.4px]' /> },
+              { label: 'Caloocan City, Philippines', icon: <TbMapPin className='size-4.5 stroke-[1.4px]' /> }
+            ].map((item, index) => (
+              <span className='text-indigo-100/80 inline-flex items-center gap-2' key={index}>
+                {item.icon}
+                <p>{item.label}</p>
+              </span>
+            ))}
+          </div>
+          <Button variant="primary" label="Drop me a line" />
         </div>
         {/* Right Contents */}
-        <div className='border'>
-          s
+        <div className=''>
+          <Button variant="secondary" label="Buy me a coffee" />
         </div>
       </div>
       {/* Footer */}
       <footer className='h-auto w-full px-6'>
-        <div className='overflow-hidden relative px-[1.5px] pt-[1.5px] footer-container-glow w-full h-full rounded-t-4xl'>
+        <div className='overflow-hidden relative px-[1.5px] pt-[1.5px] footer-container-glow w-full rounded-t-4xl'>
           {/* Glow Try */}
           <img src={CertificationsGlow} alt="Glow" className='absolute left-0 w-full h-full scale-x-[-1]' />
-          <div className='flex justify-between bg-[#17151E] w-full h-full rounded-t-4xl md:px-30 px-2 md:py-24 py-9'>
+          <div className='flex justify-between bg-[#17151E] w-full h-full rounded-t-4xl md:px-30 px-2 md:py-16 py-9'>
             {/* Left Content */}
-            <div className='border flex gap-8 z-20'>
+            <div className='flex flex-col gap-8 z-20'>
               {/* Logo */}
               <div className='size-[120px] rounded-[33px] p-[2px] bg-gradient-to-b from-[#BFADEF] to-[#4F46E5] shadow-2xl shadow-indigo-500/20 z-10'>
                 <div className='grid place-content-center w-full h-full bg-[#191726] rounded-[32px]'>
                   <img src={FMLogo} alt="FM Logo" className='w-[40px] h-auto' />
                 </div>
               </div>
-              <h4>Frederick Moreno</h4>
+              <div className='space-y-1'>
+                <span className='flex items-center gap-1 group'>
+                  <h2>FM © 2025</h2>
+                  <TbArrowUpRight className='text-indigo-100 group-hover:text-indigo-400 transition-smooth size-12 stroke-[1.4px]' />
+                </span>
+                <p>Developed and designed by <span className='text-indigo-400'>Frederick Moreno</span>.</p>
+              </div>
             </div>
             {/* Links */}
-            <div className='grid grid-cols-3 gap-x-12 z-20'>
-              {/* Link 1 */}
+            <div className='flex gap-x-20 z-20'>
+              {/* Links */}
               <div className='flex flex-col gap-y-2'>
-                <p className='pb-5'>Links</p>
-                <span className='text-sm font-bold text-[#17151E]'>Facebook</span>
-                <span className='text-sm font-bold text-[#17151E]'>Instagram</span>
-                <span className='text-sm font-bold text-[#17151E]'>Behance</span>
-                <span className='text-sm font-bold text-[#17151E]'>Dribbble</span>
+                <span className='pb-5 text-md text-[#17151E] font-bold'>Navigations</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Home</span>
+                <span className='text-sm font-semibold text-[#17151E]'>About</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Experience</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Expertise</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Projects</span>
               </div>
-              {/* Link 1 */}
+              {/* Socials */}
               <div className='flex flex-col gap-y-2'>
-                <p className='pb-5'>Links</p>
-                <span className='text-sm font-bold text-[#17151E]'>Facebook</span>
-                <span className='text-sm font-bold text-[#17151E]'>Instagram</span>
-                <span className='text-sm font-bold text-[#17151E]'>Behance</span>
-                <span className='text-sm font-bold text-[#17151E]'>Dribbble</span>
-              </div>
-              {/* Link 1 */}
-              <div className='flex flex-col gap-y-2'>
-                <p className='pb-5'>Links</p>
-                <span className='text-sm font-bold text-[#17151E]'>Facebook</span>
-                <span className='text-sm font-bold text-[#17151E]'>Instagram</span>
-                <span className='text-sm font-bold text-[#17151E]'>Behance</span>
-                <span className='text-sm font-bold text-[#17151E]'>Dribbble</span>
+                <span className='pb-5 text-md text-[#17151E] font-bold'>Socials</span>
+                <span className='text-sm font-semibold text-[#17151E]'>LinkedIn</span>
+                <span className='text-sm font-semibold text-[#17151E]'>GitHub</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Behance</span>
+                <span className='text-sm font-semibold text-[#17151E]'>Dribbble</span>
+                <span className='text-sm font-semibold text-[#17151E]'>TikTok</span>
               </div>
             </div>
           </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -130,48 +130,50 @@ const Experience = () => {
   const Certifications = () => {
     return (
       <div className='h-94 w-full px-6'>
-        <div className='relative border border-[#2d283d] bg-transparent h-full rounded-4xl grid md:grid-cols-2 grid-cols-1 overflow-hidden'>
-          {/* Left Glow */}
-          <img src={CertificationsGlow} alt="Left Glow" className='absolute left-0 h-full w-auto z-10' />
-          {/* Right Glow */}
-          <div className='absolute right-0 bottom-0 size-64 rounded-lg bg-indigo-500/20 blur-3xl z-10' />
-          {/* Left Content */}
-          <div className='z-20 p-30 space-y-4'>
-            <span className='block text-4xl text-[#15131C] font-bold'>certifications.</span>
-            <span className='block text-sm text-[#15131C] font-bold w-full max-w-xs'>Lorem, ipsum dolor sit amet consecur adipicing elit. Nisi beatae nobis nihil minima.</span>
-            <Button variant="secondary" label="View certificates" />
-          </div>
-          {/* Right Content */}
-          <div className='grid grid-cols-3 gap-7 lg:pr-16 md:pr-0'>
-            {/* First Column */}
-            <div className='border flex flex-col justify-start items-start gap-2 -skew-x-36 -mt-20'>
-              <div id='bottom' className='h-auto border' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+        <div className='p-[1.5px] h-full certis-container-glow rounded-4xl'>
+          <div className='relative bg-[#1d1b28] w-full h-full rounded-[31px] grid md:grid-cols-2 grid-cols-1 overflow-hidden'>
+            {/* Left Glow */}
+            <img src={CertificationsGlow} alt="Left Glow" className='absolute left-0 h-full w-auto z-10' />
+            {/* Right Glow */}
+            <div className='absolute right-0 bottom-0 size-64 rounded-lg bg-indigo-500/20 blur-3xl z-10' />
+            {/* Left Content */}
+            <div className='z-20 p-30 space-y-4'>
+              <span className='block text-4xl text-[#15131C] font-bold'>certifications.</span>
+              <span className='block text-sm text-[#15131C] font-bold w-full max-w-xs'>Lorem, ipsum dolor sit amet consecur adipicing elit. Nisi beatae nobis nihil minima.</span>
+              <Button variant="secondary" label="View certificates" />
             </div>
-            <div className='border flex flex-col gap-2 -skew-x-36 -mt-32'>
-              <div id="top" className="h-auto" />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-            </div>
-            <div className='border flex flex-col justify-start items-start gap-2 -skew-x-36 -mt-44'>
-              <div id='bottom' className='h-auto border' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
-              <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+            {/* Right Content */}
+            <div className='grid grid-cols-3 gap-7 lg:pr-16 md:pr-0'>
+              {/* First Column */}
+              <div className='border flex flex-col justify-start items-start gap-2 -skew-x-36 -mt-20'>
+                <div id='bottom' className='h-auto border' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+              </div>
+              <div className='border flex flex-col gap-2 -skew-x-36 -mt-32'>
+                <div id="top" className="h-auto" />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+              </div>
+              <div className='border flex flex-col justify-start items-start gap-2 -skew-x-36 -mt-44'>
+                <div id='bottom' className='h-auto border' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+                <img src="https://placehold.co/30x17" alt="" className='h-auto w-full bg-green-500/20' />
+              </div>
             </div>
           </div>
         </div>
@@ -180,7 +182,7 @@ const Experience = () => {
   }
 
   return (
-    <section id='experience' className='border border-blue-500 space-y-16'>
+    <section id='experience' className='border border-blue-500/20 space-y-16'>
       {/* Education and Work */}
       <EducationAndWork />
       {/* Certifications */}
