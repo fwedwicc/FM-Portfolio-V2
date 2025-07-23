a62278f5d0f6ce9cfdec50e44d96c298139c16ed
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-07-23 22:27:48 +0800

    Enhance About component with GSAP animations for headings; add SplitText for line animations and update CSS for animation layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { TbCode } from "react-icons/tb"
+import SplitText from 'gsap/SplitText'
+import { TbVector, TbCode, TbPiano, TbPencil, TbKeyboard } from "react-icons/tb"
 import { ProfileBlur } from '../assets/about'
 
 gsap.registerPlugin(ScrollTrigger)
@@ -51,6 +52,38 @@ const About = () => {
       }
     )
 
+    // === Animate h2s with id "text-animation" ===
+    const headings = gsap.utils.toArray('#text-animation')
+
+    headings.forEach((heading) => {
+      const split = new SplitText(heading, {
+        type: 'lines',
+        linesClass: 'split-line'
+      })
+
+      // Wrap each line in a container (optional but can help with layout)
+      split.lines.forEach(line => {
+        const wrapper = document.createElement('div')
+        wrapper.classList.add('split-parent')
+        line.parentNode.insertBefore(wrapper, line)
+        wrapper.appendChild(line)
+      })
+
+      gsap.from(split.lines, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 0.6,
+        ease: 'circ.out',
+        stagger: 0.15,
+        scrollTrigger: {
+          trigger: heading,
+          start: 'top 90%',
+          toggleActions: 'play none none reverse',
+          markers: false,
+        },
+      })
+    })
+
   }, [])
 
   const GlowContainer = ({ id, variant, children, styles }) => {
@@ -75,8 +108,8 @@ const About = () => {
         <GlowContainer id='left-container' variant='left' styles='lg:col-span-2 col-span-auto'>
           <div className='p-8 w-full h-full bg-[#15131C] rounded-[31px]'>
             <div className='space-y-4'>
-              <h2>who am i?</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quosorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos c culpa, porro reiciendis cumque quasi.</p>
+              <h2 id='text-animation'>who am i?</h2>
+              <p id='text-animation'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quosorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos corem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos c culpa, porro reiciendis cumque quasi.</p>
             </div>
           </div>
         </GlowContainer>
@@ -84,13 +117,13 @@ const About = () => {
         <GlowContainer id='right-container' variant='right'>
           <div className='relative w-full lg:h-full h-64 bg-[#15131C] rounded-[31px] overflow-hidden'>
             {/* Image */}
-            <img src="https://avatars.githubusercontent.com/u/149291942?v=4" alt="Sample Profile" className='absolute top-0 left-0 object-cover w-full h-full' />
+            <img src="https://avatars.githubusercontent.com/u/149291942?v=4" alt="Sample Profile" className='absolute top-0 left-0 object-cover w-full h-full opacity-40' />
             {/* Blur Effect */}
-            <img src={ProfileBlur} alt="Profile Blur Effect" className='absolute w-[80%] -bottom-24 z-20 border border-green-500' />
+            {/* <img src={ProfileBlur} alt="Profile Blur Effect" className='absolute w-[80%] -bottom-24 z-50 border border-green-500' /> */}
             {/* Content */}
             <div className='absolute w-full bottom-0 p-8'>
-              <h4>Frederick Moreno</h4>
-              <p>Currently eating noodles.</p>
+              <h4 id='text-animation'>Frederick Moreno</h4>
+              <p id='text-animation'>Currently eating noodles.</p>
             </div>
           </div>
         </GlowContainer>
@@ -98,13 +131,11 @@ const About = () => {
         <GlowContainer id='left-container' variant='left'>
           <div className='p-8 w-full h-full bg-[#15131C] rounded-[31px]'>
             <div className='space-y-4'>
-
               <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] shadow-[#110f17] group-hover:shadow-xl transition-smooth'>
-                <TbCode className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1.2px] transition-smooth' />
+                <TbVector className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1.2px] transition-smooth' />
               </div>
-
-              <h2>i design.</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
+              <h2 id='text-animation'>i design.</h2>
+              <p id='text-animation'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
             </div>
           </div>
         </GlowContainer>
@@ -115,8 +146,11 @@ const About = () => {
           {/* Content */}
           <div className='p-8 w-full h-full bg-[#15131C] rounded-[31px]'>
             <div className='space-y-4'>
-              <h2>i develop.</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
+              <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] shadow-[#110f17] group-hover:shadow-xl transition-smooth'>
+                <TbCode className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1.2px] transition-smooth' />
+              </div>
+              <h2 id='text-animation'>i develop.</h2>
+              <p id='text-animation'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
             </div>
           </div>
         </div>
@@ -124,8 +158,19 @@ const About = () => {
         <GlowContainer id='right-container' variant='right'>
           <div className='p-8 w-full h-full bg-[#15131C] rounded-[31px]'>
             <div className='space-y-4'>
-              <h2>beyond field.</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
+              <h2 id='text-animation'>beyond field.</h2>
+              <p id='text-animation'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus vitae quos culpa, porro reiciendis cumque quasi.</p>
+              <div className='flex gap-4'>
+                <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] shadow-[#14111a] group-hover:shadow-xl transition-smooth'>
+                  <TbPiano className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1px] transition-smooth' />
+                </div>
+                <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] shadow-[#14111a] group-hover:shadow-xl transition-smooth'>
+                  <TbPencil className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1px] transition-smooth' />
+                </div>
+                <div className='inline-flex p-4 rounded-[19px] border border-[#1d1b28] group-hover:border-[#211f2d] shadow-[#14111a] group-hover:shadow-xl transition-smooth'>
+                  <TbKeyboard className='text-indigo-200/50 group-hover:text-indigo-400 size-8 stroke-[1px] transition-smooth' />
+                </div>
+              </div>
             </div>
           </div>
         </GlowContainer>
